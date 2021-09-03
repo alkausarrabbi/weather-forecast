05b7d599dded85449afa70499985f8577c875af1
alkausarrabbi <alkausarrabbi24@gmail.com>
2021-09-03 21:12:13 +0600

    update link

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const API_KEY=`3bdd225b97e0023f7702675e8f9c7cba`;
 const searchTemperature=()=>{
     const cityInput=document.getElementById('city-name');
     const cityName=cityInput.value;
-    const url=`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res =>res.json())
     .then(data=>displayWeather(data));
@@ -15,7 +15,7 @@ const displayWeather=(temp)=>{
     innerText("city",temp.name);
     innerText("temperature",temp.main.temp);
     innerText("condition",temp.weather[0].main);
-    const url=` http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
+    const url=` https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
     const weatherIcon=document.getElementById('weather-icon');
     weatherIcon.setAttribute("src",url);
 }
